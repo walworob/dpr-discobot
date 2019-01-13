96212f443778f67c0a79aeaec5080152d602aae9
Rob Walworth <rjwalworth17@gmail.com>
2019-01-13 00:10:48 -0500

    Tag didn't work, add debug statements

--- index.js
+++ index.js
@@ -129,6 +129,8 @@ bot.on('voiceStateUpdate', (oldMember, newMember) => {
     // channel from another voice channel.
     if (oldUserChannel === undefined && newUserChannel !== undefined) {
         let voiceChannel = newMember.voiceChannel;
+        console.log(newMember.user.toString());
+        newMember.send(newMember.user.toString());
         //newMember.sendMessage(newMember.user.toString());
 
         // TODO:    Get everyone's username so these can be replaced with newMember.user
